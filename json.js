7e3348bb1368cbf5a93d45f8ee051561206ee0ec
daniyelrub <daniyalruban1999@gmail.com>
2024-04-23 12:38:32 +0530

    added loop method

--- json.js
+++ json.js
@@ -3,67 +3,127 @@
 
 
 
-
 const resumeData = {
-    "name": "Danielruban",
-    "age": 23,
-    "gender": "male",
-    "10th-mark":"460",
-    "10th-persentage": 92,
-    "12th-persentage": 977,
-    "12th-persentage": 81,
-    "u.g-degree":"b.sc-mathematics",
-    "height": "1.72cm",
-    "weight": 56,
-    "color": "white",
-    "date_of_birth": "10.10.2000",
-    "education": "MSc (Mathematics)",
-    "university": "Alagappa University (Karaikudi)",
-    "location": "Rameswaram",
-    "college_name": "Govt Arts and Science College",
-    "study_skills": ["HTML", "CSS", "JavaScript", "Excel"],
-    "sports_skills": ["cricket", "football", "basketball"]
-  };
-  
-
-
-
-  //  for the above the JSON  format  iterate over all for loops(for,for in ,for of,for each)
-
-
-
-  //for loop
-  for (let key in resumeData) {
-    console.log(key + ': ' + resumeData[key]);
-  }
-  
-
-
-//for in loop
-  for (const key in resumeData) {
-    if (resumeData.hasOwnProperty(key)) {
-      console.log(key + ': ' + resumeData[key]);
+  name: "Danielruban",
+  age: 23,
+  gender: "male",
+  state: "Tamil Nadu",
+  district: "Ramanathapuram",
+  landmark: "Thondi",
+  education: [
+    {
+      year: "2021-2023",
+      institution: "Alagappa University, Karaikudi",
+      degree: "MSc (Mathematics)",
+      percentage: "77.10"
+    },
+    {
+      year: "2017-2020",
+      institution: "Alagappa University, Karaikudi",
+      degree: "BSc (Mathematics)",
+      percentage: "70.40"
+    },
+    {
+      year: "2017",
+      institution: "De Britto Hr. Sec. School",
+      degree: "HSC",
+      percentage: "64.25"
     }
+  ]
+};
+
+
+
+//  for the above the JSON  format  iterate over all for loops(for,for in ,for of,for each)
+
+// Using this (for loop)
+for (let key in resumeData) {
+  if (Array.isArray(resumeData[key])) {
+    console.log(key + ": ");
+    for (let i = 0; i < resumeData[key].length; i++) {
+      for (let prop in resumeData[key][i]) {
+        console.log("  " + prop + ": " + resumeData[key][i][prop]);
+      }
+    }
+  } else {
+    console.log(key + ": " + resumeData[key]);
   }
+}
+
+
+//using this (for in loop)
+for (let key in resumeData) {
+  if (Array.isArray(resumeData[key])) {
+    console.log(key + ": ");
+    resumeData[key].forEach((item) => {
+      for (let prop in item) {
+        console.log("  " + prop + ": " + item[prop]);
+      }
+    });
+  } else {
+    console.log(key + ": " + resumeData[key]);
+  }
+}
+
   
-
-
-
-//for of loop
-const keys = Object.keys(resumeData);
-for (const key of keys) {
-  console.log(key + ': ' + resumeData[key]);
+//using this (for of loop)
+for (let key in resumeData) {
+  if (Array.isArray(resumeData[key])) {
+    console.log(key + ": ");
+    for (let item of resumeData[key]) {
+      for (let prop in item) {
+        console.log("  " + prop + ": " + item[prop]);
+      }
+    }
+  } else {
+    console.log(key + ": " + resumeData[key]);
+  }
 }
 
 
 
+//using this(for each loop)
+for (let key in resumeData) {
+  if (Array.isArray(resumeData[key])) {
+    console.log(key + ": ");
+    resumeData[key].forEach((item) => {
+      for (let prop in item) {
+        console.log("  " + prop + ": " + item[prop]);
+      }
+    });
+  } else {
+    console.log(key + ": " + resumeData[key]);
+  }
+}
 
-//for each loop
-Object.entries(resumeData).forEach(([key, value]) => {
-    console.log(key + ': ' + value);
-  });
   
 
 
+// //this is below here simple loop method in JSON format
+
+// //for in loop
+//   for (const key in resumeData) {
+//     if (resumeData.hasOwnProperty(key)) {
+//       console.log(key + ': ' + resumeData[key]);
+//     }
+//   }
+  
+
+
+
+// //for of loop
+// const keys = Object.keys(resumeData);
+// for (const key of keys) {
+//   console.log(key + ': ' + resumeData[key]);
+// }
+
+
+
+
+// //for each loop
+// Object.entries(resumeData).forEach(([key, value]) => {
+//     console.log(key + ': ' + value);
+//   });
+  
 
 
